2120575f1b7fb9cadf89ac92de085a201c2240a4
Anton K <wiistriker@gmail.com>
2020-11-06 10:08:45 +0300

    error code in WiracleAPIException

--- src/API/WiracleAPIException.php
+++ src/API/WiracleAPIException.php
@@ -1,8 +1,24 @@
 <?php
-
 namespace JustCommunication\WiracleSDK\API;
 
 class WiracleAPIException extends \Exception
 {
+    /**
+     * @var string|int
+     */
+    protected $error_code;
 
+    public function __construct($message, $error_code = null)
+    {
+        $this->error_code = $error_code;
+        parent::__construct($message);
+    }
+
+    /**
+     * @return int|string
+     */
+    public function getErrorCode()
+    {
+        return $this->error_code;
+    }
 }
